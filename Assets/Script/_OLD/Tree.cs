e129ec521ddc9488fc0898ca59aa9057fa681eff
Jia Xing Wan <jxwan98@gmail.com>
2022-08-16 22:34:59 +0800

    Change Tree sprite to House after TreeDie animation finished

--- Assets/Script/_OLD/Tree.cs
+++ Assets/Script/_OLD/Tree.cs
@@ -8,10 +8,24 @@ public class Tree : MonoBehaviour
     private SpriteRenderer _renderer;
     private Animator _animator;
 
+    private bool _isAnimationCompleted = false;
+
     private void Awake()
     {
         _renderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
+    }
+
+    void LateUpdate()
+    {
+        if (_isAnimationCompleted)
+        {
+            _renderer.sprite = AddressableManager.LoadSprite("House");
+            gameObject.AddComponent<Rigidbody2D>();
+            gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+            _animator.enabled = false;
+            _isAnimationCompleted = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -27,7 +41,13 @@ public class Tree : MonoBehaviour
         await Task.Delay(500);
         
         SoundManager.Instance.PlayTreeSfx(Settings.SoundType.TreeSpirit);
-        await Task.Delay(500);
-        _renderer.sprite = ResourceManager.LoadSprite("House");
+    }
+
+    public void AnimationCompleted()
+    {
+        _isAnimationCompleted = true;
+        var sprite = AddressableManager.LoadSprite("House");
+        Debug.Log(sprite.name);
+        _renderer.sprite = AddressableManager.LoadSprite("House");
     }
 }
