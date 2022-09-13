6cc1a96f90961691e44fdc012a87ca56108fe485
Jia Xing Wan <jxwan98@gmail.com>
2022-09-13 22:01:11 +0800

    Fire fade out

--- Assets/Script/Objects/Fire.cs
+++ Assets/Script/Objects/Fire.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Fire : MonoBehaviour
 {
+    private SpriteRenderer _spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        TractorStopPoint.OnTractorStops += FireStartFadingOut;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FireStartFadingOut()
+    {
+        _spriteRenderer.DOFade(0.0f, 5.0f);
     }
 }
